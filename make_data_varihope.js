5cf64fd09e378b93fd80bafa9c6a6394554a287f
pielchoisukjune <john@pielkorea.com>
2020-07-13 21:07:47 +0900

    .

--- make_data_varihope.js
+++ make_data_varihope.js
@@ -55,43 +55,64 @@ var getNewToken = function getNewToken( oAuth2Client, callback ){
 	});
   }
 
+var spreadsheetId = "1F2AlIXsDem5POlAcCnrnIj6zuj_gJza7wpS7C8TbZ7A";
 var result_path = "./brand/varihope/"
 var OPTIONS = {
 	FUNC00 : {
-		spreadsheetId : '1F2AlIXsDem5POlAcCnrnIj6zuj_gJza7wpS7C8TbZ7A'
+		spreadsheetId : spreadsheetId
 		, range : '마케팅집행리스트!A:M'
 	}
 	, FUNC01 : {
-		spreadsheetId : '1F2AlIXsDem5POlAcCnrnIj6zuj_gJza7wpS7C8TbZ7A'
+		spreadsheetId : spreadsheetId
 		, range : '페이스북 STATISTIC!A:M'
 	}
 	, FUNC02 : {
-		spreadsheetId : '1F2AlIXsDem5POlAcCnrnIj6zuj_gJza7wpS7C8TbZ7A'
+		spreadsheetId : spreadsheetId
 		, range : '페이스북 연령 통계!A:M'
 	}
 	, FUNC03 : {
-		spreadsheetId : '1F2AlIXsDem5POlAcCnrnIj6zuj_gJza7wpS7C8TbZ7A'
+		spreadsheetId : spreadsheetId
 		, range : '페이스북 지역별 통계!A:M'
 	}
 	, FUNC04 : {
-		spreadsheetId : '1F2AlIXsDem5POlAcCnrnIj6zuj_gJza7wpS7C8TbZ7A'
+		spreadsheetId : spreadsheetId
 		, range : '페이스북 시간대별 통계!A:M'
 	}
 	, FUNC05 : {
-		spreadsheetId : '1F2AlIXsDem5POlAcCnrnIj6zuj_gJza7wpS7C8TbZ7A'
+		spreadsheetId : spreadsheetId
 		, range : 'Manger Insight!A:M'
 	}
 	, FUNC06 : {
-		spreadsheetId : '1F2AlIXsDem5POlAcCnrnIj6zuj_gJza7wpS7C8TbZ7A'
+		spreadsheetId : spreadsheetId
 		, range : '지역코드!A:M'
 	}
 	, FUNC07 : {
-		spreadsheetId : '1F2AlIXsDem5POlAcCnrnIj6zuj_gJza7wpS7C8TbZ7A'
+		spreadsheetId : spreadsheetId
 		, range : 'KOLs!A:M'
 	}
 	, FUNC08 : {
-		spreadsheetId : '1F2AlIXsDem5POlAcCnrnIj6zuj_gJza7wpS7C8TbZ7A'
+		spreadsheetId : spreadsheetId
 		, range : '월간진행수량!A:M'
+	}
+	, FUNC09 : {
+		spreadsheetId : spreadsheetId
+		, range : '구글광고통계!A:M'
+	}
+	, FUNC10 : {
+		spreadsheetId : spreadsheetId
+		, range : '구글 시간대 보고서!A:Z'
+	}
+	, FUNC11 : {
+		spreadsheetId : spreadsheetId
+		, range : '구글 컴페인별 연령대 보고서!A:Z'
+	}
+	, FUNC12 : {
+		spreadsheetId : spreadsheetId
+		, range : 'Google Top SEO Article!A:Z'
+	}
+	, FUNC13 : {
+		spreadsheetId : spreadsheetId
+		, range : '구글광고키워드!A:Z'
 	}
 }
 
@@ -106,6 +127,11 @@ var d = {
 	, geocode : {}
 	, kols : {}
 	, statistic_monthly : []
+	, google_total : {}
+	, google_time : {}
+	, google_ages : {}
+	, google_seo_list : {}
+	, google_ad_info_list : {}
 };
 
 //-------------------------------------------------------;
@@ -478,8 +504,233 @@ var FUNC08 = function( auth ){
 		}
 
 		console.log( "[ E ] - FUNC08();" )
+		FUNC09( auth );
+	});
+}
+
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+var FUNC09 = function( auth ){
+	var key = "FUNC09"
+
+	console.log( "[ S ] - FUNC09();" )
+
+	google.sheets({version: 'v4', auth}).spreadsheets.values.get( OPTIONS[ key ], function(err, res){
+		if (err) return console.log('The API returned an error: ' + err);
+		const rows = res.data.values;
+		if (rows.length == 0 ) return console.log('ads list - No data found.');
+
+		var i = 0,iLen = rows.length,io;
+		var _header = [];
+		for(;i<iLen;++i){
+			io = rows[ i ];
+			if( i == 0 )
+			{
+				io.forEach(function( item ){ _header.push( item ) });
+				//console.log( _header )
+			}
+			else
+			{
+				var z = 0,zLen = io.length,zo;
+				var o = {};
+				for(;z<zLen;++z){
+					zo = io[ z ];
+					o[ _header[ z ] ] = zo;
+				}
+//				if( !d.google_total[ io[0] ] ) d.google_total[ io[0] ] = [];
+//				d.google_total[ io[0] ].push( o )
+				if( !d.google_total[ io[0] ] ) d.google_total[ io[0] ] = {};
+				d.google_total[ io[0] ][ io[ 1 ] ] = o;
+
+			}
+		}
+		console.log( "[ E ] - FUNC09();" )
+		FUNC10( auth )
+	});
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+var FUNC10 = function( auth ){
+	var key = "FUNC10"
+
+	console.log( "[ S ] - FUNC10();" )
+
+	google.sheets({version: 'v4', auth}).spreadsheets.values.get( OPTIONS[ key ], function(err, res){
+		if (err) return console.log('The API returned an error: ' + err);
+		const rows = res.data.values;
+		if (rows.length == 0 ) return console.log('ads list - No data found.');
+
+		var i = 0,iLen = rows.length,io;
+		var _header = [];
+		for(;i<iLen;++i){
+			io = rows[ i ];
+			if( i == 0 )
+			{
+				io.forEach(function( item ){ _header.push( item ) });
+				//console.log( _header )
+			}
+			else
+			{
+				var z = 0,zLen = io.length,zo;
+				var o = {};
+				for(;z<zLen;++z){
+					zo = io[ z ];
+					o[ _header[ z ] ] = zo;
+				}
+				if( !d.google_time[ io[0] ] ) d.google_time[ io[0] ] = [];
+				d.google_time[ io[0] ].push( o )
+			}
+		}
+		console.log( "[ E ] - FUNC10();" )
+		FUNC11( auth )
+	});
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+var FUNC11 = function( auth ){
+	var key = "FUNC11"
+
+	console.log( "[ S ] - FUNC11();" )
+
+	google.sheets({version: 'v4', auth}).spreadsheets.values.get( OPTIONS[ key ], function(err, res){
+		if (err) return console.log('The API returned an error: ' + err);
+		const rows = res.data.values;
+		if (rows.length == 0 ) return console.log('ads list - No data found.');
+
+		var i = 0,iLen = rows.length,io;
+		var _header = [];
+		for(;i<iLen;++i){
+			io = rows[ i ];
+			if( i == 0 )
+			{
+				io.forEach(function( item ){ _header.push( item ) });
+				//console.log( _header )
+			}
+			else
+			{
+				var z = 0,zLen = io.length,zo;
+				var o = {};
+				for(;z<zLen;++z){
+					zo = io[ z ];
+					o[ _header[ z ] ] = zo;
+				}
+				if( !d.google_ages[ io[0] ] ) d.google_ages[ io[0] ] = [];
+				d.google_ages[ io[0] ].push( o )
+			}
+		}
+		console.log( "[ E ] - FUNC11();" )
+		FUNC12( auth )
+	});
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+var FUNC12 = function( auth ){
+	var key = "FUNC12"
+
+	console.log( "[ S ] - FUNC12();" )
+
+	google.sheets({version: 'v4', auth}).spreadsheets.values.get( OPTIONS[ key ], function(err, res){
+		if (err) return console.log('The API returned an error: ' + err);
+		const rows = res.data.values;
+		if (rows.length == 0 ) return console.log( key + ' -- No data found.');
+
+		var i = 0,iLen = rows.length,io;
+		var _header = [];
+		for(;i<iLen;++i){
+			io = rows[ i ];
+			if( i == 0 )
+			{
+				io.forEach(function( item ){ _header.push( item ) });
+				//console.log( _header )
+			}
+			else
+			{
+				var z = 0,zLen = io.length,zo;
+				var o = {};
+				for(;z<zLen;++z){
+					zo = io[ z ];
+					if( _header[ z ] == "구분" )
+					{
+						var _tzo = zo.trim();
+						if( !d.google_seo_list[ io[0] ] )
+						{
+							d.google_seo_list[ io[0] ] = {};
+						}
+
+						if( !d.google_seo_list[ io[0] ][ _tzo ] )
+						{
+							d.google_seo_list[ io[0] ][ _tzo ] = [];
+						}
+					}
+					o[ _header[ z ] ] = zo.trim();
+				}
+				d.google_seo_list[ io[0] ][ o[ "구분" ] ].push( o )
+			}
+		}
+		console.log( "[ E ] - FUNC12();" )
+		FUNC13( auth );
+	});
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+var FUNC13 = function( auth ){
+	var key = "FUNC13"
+
+	console.log( "[ S ] - FUNC13();" )
+
+	google.sheets({version: 'v4', auth}).spreadsheets.values.get( OPTIONS[ key ], function(err, res){
+		if (err) return console.log('The API returned an error: ' + err);
+		const rows = res.data.values;
+		if (rows.length == 0 ) return console.log('ads list - No data found.');
+
+		var i = 0,iLen = rows.length,io;
+		var _header = [];
+		for(;i<iLen;++i){
+			io = rows[ i ];
+			if( i == 0 )
+			{
+				io.forEach(function( item ){ _header.push( item ) });
+				//console.log( _header )
+			}
+			else
+			{
+				var z = 0,zLen = io.length,zo;
+				var o = {};
+				for(;z<zLen;++z){
+					zo = io[ z ];
+					o[ _header[ z ] ] = zo;
+				}
+//				if( !d.google_ad_info_list[ io[0] ] ) d.google_ad_info_list[ io[0] ] = [];
+//				d.google_ad_info_list[ io[0] ].push( o )
+				if( !d.google_ad_info_list[ io[0] ] ) d.google_ad_info_list[ io[0] ] = {};
+				d.google_ad_info_list[ io[0] ][ io[ 1 ] ] = o;
+
+			}
+		}
+		console.log( "[ E ] - FUNC13();" )
 		fs.writeFileSync( result_path + "data.json", JSON.stringify( d, null, 4 ), { flag : "w"});
 	});
 }
+
 
 authorize( credentail, FUNC00 );
